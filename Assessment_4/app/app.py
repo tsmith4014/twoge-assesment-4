e7eee7773fda55df4839966e5b64349b3146951d
tsmith4014 <chjthomps@gmail.com>
2024-02-19 08:53:37 -0500

    completed

--- Assessment_4/app/app.py
+++ Assessment_4/app/app.py
@@ -89,4 +89,4 @@ def health_check():
     return "Healthy", 200
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=8080)
+    app.run(debug=True,host='0.0.0.0', port=8080) #set debug to False for prod
